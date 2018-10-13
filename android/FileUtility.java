40456f4e5290bbce6a3ec7f96d9b479b44d470ce
andrew-madden <andrew.madden@live.com.au>
2018-10-14 01:50:02 +1100

    overloaded methods to include File, included read inputstream option

--- android/FileUtility.java
+++ android/FileUtility.java
@@ -1,13 +1,17 @@
 import android.util.Log;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Iterator;
+
 
 /**
  * Provides file utility methods for creating arrays of strings when reading from files, and
@@ -47,6 +51,56 @@ public class FileUtility {
     }
 
     /**
+     * Reads data from file returning the lines as a list, or null if error
+     * @param  fileName file object
+     * @return          collection of strings
+     */
+    public static ArrayList<String> readFromFile(File fileName) {
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(fileName));
+            ArrayList<String> data = new ArrayList<String>();
+            String line = reader.readLine();
+            while (line != null) {
+                data.add(line);
+                line = reader.readLine();
+            }
+            reader.close();
+            return data;
+
+        } catch (FileNotFoundException e) {
+            Log.e(TAG, "readFromFile: " + e.getMessage());
+        } catch (IOException e) {
+            Log.e(TAG, "readFromFile: " + e.getMessage());
+        }
+        return null;
+    }
+
+    /**
+     * Reads data from file returning the lines as a list, or null if error
+     * @param  fileInputStream input stream from raw file
+     * @return          collection of strings
+     */
+    public static ArrayList<String> readFromFile(InputStream fileInputStream) {
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(fileInputStream));
+            ArrayList<String> data = new ArrayList<String>();
+            String line = reader.readLine();
+            while (line != null) {
+                data.add(line);
+                line = reader.readLine();
+            }
+            reader.close();
+            return data;
+
+        } catch (FileNotFoundException e) {
+            Log.e(TAG, "readFromFile: " + e.getMessage());
+        } catch (IOException e) {
+            Log.e(TAG, "readFromFile: " + e.getMessage());
+        }
+        return null;
+    }
+
+    /**
      * Write data to file
      * @param fileName pathname
      * @param data     collection of strings
@@ -65,7 +119,28 @@ public class FileUtility {
         }
     }
 
-      /**
+
+    /**
+     * Write data to file
+     * @param fileName file object
+     * @param data     collection of strings
+     */
+    public static void writeToFile(File fileName, ArrayList<String> data) {
+        try {
+            PrintWriter writer = new PrintWriter(new FileWriter(fileName));
+            Iterator<String> itData = data.iterator();
+            while (itData.hasNext()) {
+                writer.println(itData.next());
+            }
+            writer.close();
+
+        } catch(IOException e) {
+            Log.e(TAG, "readFromFile: " + e.getMessage());
+        }
+    }
+
+
+    /**
      * Write data to file
      * @param fileName pathname
      * @param data     collection of strings
@@ -81,6 +156,27 @@ public class FileUtility {
 
         } catch(IOException e) {
             Log.e(TAG, "readFromFile: " + e.getMessage());
+        }
+    }
+
+    /**
+     * Write data to file
+     * @param fileName file object
+     * @param data     collection of strings
+     */
+    public static boolean appendToFile(File fileName, ArrayList<String> data) {
+        try {
+            PrintWriter writer = new PrintWriter(new FileWriter(fileName, true));
+            Iterator<String> itData = data.iterator();
+            while (itData.hasNext()) {
+                writer.println(itData.next());
+            }
+            writer.close();
+            return true;
+
+        } catch(IOException e) {
+            Log.e(TAG, "readFromFile: " + e.getMessage());
+            return false;
         }
     }
 }
